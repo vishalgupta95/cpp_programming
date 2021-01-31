12c64f2e6bf77d04534834e821e551572e691e45
vishalgupta95 <77653464+vishalgupta95@users.noreply.github.com>
2021-01-31 00:18:46 -0500

    Add files via upload

--- move_constructor.cpp
+++ move_constructor.cpp
@@ -1,80 +1,73 @@
-// C++ program without declaring the
-// move constructor
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
 /*
-Move constructor moves the resources in the heap, i.e., unlike copy constructors which copy the data of the existing object and assigning it to the new object move constructor just makes the pointer of the declared object to point to the data of temporary object and nulls out the pointer of the temporary objects. Thus, move constructor prevents unnecessarily copying data in the memory.  
-
-Work of move constructor looks a bit like default member-wise copy constructor but in this case, it nulls out the pointer of the temporary object preventing more than one object to point to same memory location.
-
-Below is the program without declaring the move constructor:
+A copy constructor constructs a new object by using the content of the argument object. An
+overloaded assignment operator assigns the contents of an existing object to another existing
+object of the same class.
 */
-// Move Class
-class Move {
+
+/*In the above code, there is a serious performance overhead as the temporary object cpym is first 
+created in the default constructor and then in the copy constructor. The move constructor is 
+used to avoid this performance overhead:
+*/
+/*
+The move assignment operator is different than a move constructor because a move assignment
+ operator is called on an existing object, while a move constructor is called on an object created by the operation. 
+Thereafter, the other object's data is no longer valid.
+*/
+class Names{
 private:
-	// Declaring the raw pointer as
-	// the data member of the class
-	int* data;
-
+    string* name;
 public:
-	// Constructor
-	Move(int d)
-	{
-		// Declare object in the heap
-		data = new int;
-		*data = d;
-
-		cout << "Constructor is called for "
-			<< d << endl;
-	};
-
-	// Copy Constructor to delegated
-	// Copy constructor
-	Move(const Move& source)
-		: Move{ *source.data }
-	{
-
-		// Copying constructor copying
-		// the data by making deep copy
-		cout << "Copy Constructor is called - "
-			<< "Deep copy for "
-			<< *source.data
-			<< endl;
-	}
-
-	// Destructor
-	~Move()
-	{
-		if (data != nullptr)
-
-			// If the pointer is not
-			// pointing to nullptr
-			cout << "Destructor is called for "
-				<< *data << endl;
-		else
-
-			// If the pointer is
-			// pointing to nullptr
-			cout << "Desturctor is called"
-				<< " for nullptr"
-				<< endl;
-
-		// Free the memory assigned to
-		// data member of the object
-		delete data;
-	}
+    Names(string place_name){
+        cout << "Overloaded constructor" << endl;
+        name = new string;
+        *name = place_name;
+    }
+    //copy constructor
+    Names(Names& cpy){
+        cout << "Copy constructor" << endl;
+        name = new string;
+        *name = *cpy.name;
+    }
+    //assignment 
+    Names& operator =(const Names& cpy){
+        cout << "Assignment operator" << endl;
+        name = new string;
+        *name = *cpy.name;
+        return *this;
+    }
+	 // Move constructor
+    // It will simply shift the resources,
+    // without creating a copy
+    //move constructor
+	//With the move constructor, the copy of the temporary object of cpym is avoided
+    Names(Names&& cpym){
+        cout << "Copy move constructor" << endl;
+        name = cpym.name;
+        cpym.name = NULL;
+    }
+    //move assignment operator
+    Names& operator=(Names&& cpym){
+        cout << "Copy assignment operator" << endl;
+        delete name;
+        name = cpym.name;
+        cpym.name = NULL;
+        return *this;
+    }
+    //destructor
+    ~Names(){
+        cout << "Deallocating memory" << endl;
+        delete [] name;
+    }
 };
 
-// Driver Code
-int main()
-{
-	// Create vector of Move Class
-	vector<Move> vec;
-
-	// Inserting object of Move class
-	vec.push_back(Move{ 10 });
-	vec.push_back(Move{ 20 });
-	return 0;
-}
+int main(){
+    Names nme("Bob");
+    Names copin("something");
+    copin = nme;
+    system("pause");
+    return 0;
+} 
