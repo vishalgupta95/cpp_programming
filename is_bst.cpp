de558907f4240cd28e94d6a327c474fc6c85bd0c
vishal gupta <vishagu2@cisco.com>
2021-05-01 21:00:36 -0700

    vishal gupta

--- is_bst.cpp
+++ is_bst.cpp
@@ -20,7 +20,39 @@ class node
         this->left = NULL; 
         this->right = NULL; 
     } 
-}; 
+};
+/*
+1) Do In-Order Traversal of the given tree and store the result in a temp array.
+3) Check if the temp array is sorted in ascending order, if it is, then the tree is BST.
+
+Time Complexity: O(n)
+
+We can avoid the use of Auxiliary Array. While doing In-Order traversal, we can keep track of previously visited node. If the value of the currently visited node is less than the previous value, then tree is not BST. Thanks to ygos for this space optimization.
+*/
+
+
+
+bool isBST(struct node* root) 
+{ 
+    static struct node *prev = NULL; 
+      
+    // traverse the tree in inorder fashion and keep track of prev node 
+    if (root) 
+    { 
+        if (!isBST(root->left)) 
+          return false; 
+  
+        // Allows only distinct valued nodes  
+        if (prev != NULL && root->data <= prev->data) 
+          return false; 
+  
+        prev = root; 
+  
+        return isBST(root->right); 
+    } 
+  
+    return true; 
+}  
   
 int isBSTUtil(node* node, int min, int max);  
   
