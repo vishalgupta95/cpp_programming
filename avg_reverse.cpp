93dab83f386e68550cb34a33bd76ab610ff5cc3b
vishal gupta <vishagu2@cisco.com>
2022-01-06 15:10:08 -0800

    brief

--- avg_reverse.cpp
+++ avg_reverse.cpp
@@ -4,41 +4,29 @@
 
 using namespace std;
 
-void disp(int *ptr, int count)
+void disp(int *top, int count)
 {
     int i = 0;
-    //sort(ptr, ptr + count);
+    int *ptr =top;
     sort(ptr, ptr + count, greater<int>()); // reverse
-
-    unique(ptr, ptr + count);
 
     for (i = 0; i < count; i++)
     {
         printf(" [%u] ", *ptr);
         ptr++;
     }
-    printf("\n\n");
-}
 
-double getAverage(int x[], int count)
-{
-    int i = 0;
-    double j;
-    for (i = 0; i < count; i++)
-    {
-        j += x[i];
-    }
-    return (j / count);
+
+
+    printf("\n\n");
 }
 
 int main()
 {
     /* an int array with 5 elements */
-    int balance[] = {1000, 2, 3, 17, 50};
-    double avg;
+    int balance[] = {1000, 2, 3, 17, 50, 3, 19, 17, 14, 34};
     int x = sizeof(balance) / sizeof(balance[0]);
-    printf("count =%d \n", x);
-    /* pass pointer to the array as an argument */
+    printf("val =%d \n", x);
     disp(balance, x);
 
     return 0;
